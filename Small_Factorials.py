ae1dc20b42ce8d2f05f7878157d9ddcb3bbac4b7
Jahnavi Saragada <120642580+21A91A04D4@users.noreply.github.com>
2023-07-02 21:16:24 +0530

    n=int(input()) for i in range(n):     s=int(input())     fact = 1     if s == 0 or s == 1:         print("1")     else:         for i in range(1, s + 1):             fact = fact * i         print(fact)

--- Small_Factorials.py
+++ Small_Factorials.py
@@ -1,6 +1,10 @@
-import math
 n=int(input())
-c=1
 for i in range(n):
     s=int(input())
-    print(math.factorial(s))
+    fact = 1
+    if s == 0 or s == 1:
+        print("1")
+    else:
+        for i in range(1, s + 1):
+            fact = fact * i
+        print(fact)
