bcac9b7f0c9da95943c05a3c73a866c568eb6195
Jahnavi Saragada <120642580+21A91A04D4@users.noreply.github.com>
2023-07-07 10:08:46 +0530

    n,m=map(int,input().split()) s=list(map(int,input().split())) l=list(map(int,input().split())) a=[] b=[] c=[] for i in s:     if(i not in a):         a.append(i) for j in l:     if(j not in b):         b.append(j) for k in a:     if(k in b):         c.append(k) print(len(c))

---
+++ Count_elements_I.py
@@ -0,0 +1,16 @@
+n,m=map(int,input().split())
+s=list(map(int,input().split()))
+l=list(map(int,input().split()))
+a=[]
+b=[]
+c=[]
+for i in s:
+    if(i not in a):
+        a.append(i)
+for j in l:
+    if(j not in b):
+        b.append(j)
+for k in a:
+    if(k in b):
+        c.append(k)
+print(len(c))
