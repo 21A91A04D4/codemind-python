ec81ef9daf883c906f26a10b8d3a272e71ce8be6
Jahnavi Saragada <120642580+21A91A04D4@users.noreply.github.com>
2023-07-12 12:23:14 +0530

    a=input() b=input() c=a.lower() d=b.lower() s=[] for i in c:     if(i not in d and i not in s and i != " "):         s.append(i) for i in d:     if(i not in c and i not in s and i != " "):         s.append(i) print(len(s))

---
+++ Count_of_non_common_characters.py
@@ -0,0 +1,12 @@
+a=input()
+b=input()
+c=a.lower()
+d=b.lower()
+s=[]
+for i in c:
+    if(i not in d and i not in s and i != " "):
+        s.append(i)
+for i in d:
+    if(i not in c and i not in s and i != " "):
+        s.append(i)
+print(len(s))
